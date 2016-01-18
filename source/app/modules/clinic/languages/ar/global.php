c1543231f0a63adb74b0f9237602126ef825f537
Esra2ahmed <esraa.ahmad@brightery.com>
2016-01-18 15:49:15 +0200

    test

--- source/app/modules/clinic/languages/ar/global.php
+++ source/app/modules/clinic/languages/ar/global.php
@@ -9,5 +9,21 @@ return [
     'menu_reservations' => 'الحجوزات',
     'menu_reservation_types' => 'نوع الحجز',
     'menu_doctor_reviews' => 'التقيمات',
+    'permissions_manage_branch' => 'إذن إدارة الفروع',
+    'permissions_manage_specification' => 'إذن إدارة التخصصات',
+    'permissions_manage_doctor' => 'إذن إدارة الأطباء',
+    'permissions_doctor_schedule' => 'إذن إدارة جداول الأطباء',
+    'permissions_doctor_schedule_exceptions' => 'إذن إدارة استثناءات جداول الأطباء',
+    'permissions_manage_users' => 'إذن إدارة المستخدمين',
+    'permissions_manage_patient_disease' => 'إذن إدارة أمراض المرضى',
+    'permissions_manage_patient_xray' => 'إذن إدارة إشاعات المرضى',
+    'permissions_manage_patient_notes' => 'إذن إدارة الملاحاظات الخاصة بالمرضى',
+    'permissions_manage_diseases' => 'إدارة الأمراض',
+    'permissions_reservations' => 'إدارة الحجوزات',
+    'permissions_reservation_types' => 'إدارة انواع الحجوزات',
+    'permissions_frontend_edit_user_profile' => 'تعديل الصفحة الشخصية ',
+    'permissions_frontend_reservations' => 'إدارة الحجوزات',
+    'permissions_frontend_doctor_schedule' => 'إدارة جداول مواعيد الأطباء',
+    'permissions_manage_welcome_screen'  => 'الشاشة الترحيبية'
 
 ];
